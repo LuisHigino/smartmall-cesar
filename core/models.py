e0e66ad2f51ffdc327f46219d74674f150ae0139
eduardommb <lemb@cesar.school>
2026-03-28 17:34:46 -0300

    fix: corrige formatacao length

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Produto(models.Model):
-    nome = models.CharField('Nome', max_lengt=200)
+    nome = models.CharField('Nome', max_length=200)
     preco = models.DecimalField('Preco', max_digits=10, decimal_places=2)
     estoque = models.IntegerField('estoque', max_digits=1000000,)
     descricao = models.TextField('Descrição', blank=True, null=True)
