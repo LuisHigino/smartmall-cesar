6ed6deac9d826aba7bdd9722ac540dd8a4b3bc76
eliancunha <eliangacunha@gmail.com>
2026-03-28 17:43:44 -0300

    forms.py

--- core/models.py
+++ core/models.py
@@ -3,14 +3,13 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField('Nome', max_lengt=200)
     preco = models.DecimalField('Preco', max_digits=10, decimal_places=2)
-    estoque = models.IntegerField('estoque', max_digits=1000000,)
+    estoque = models.IntegerField('Estoque', max_digits=1000000,)
     descricao = models.TextField('Descrição', blank=True, null=True)
 
     def __str__(self):
         return self.nome
-
-
-
+    
+  
 
 
 
