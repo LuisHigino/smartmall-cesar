41d0d08b5538cad1790447abd533198fdaa1f59f
eliancunha <eliangacunha@gmail.com>
2026-03-28 17:03:46 -0300

    add estoque

--- core/models.py
+++ core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Produto(models.Model)
+class Produto(models.Model):
     nome = models.CharField('Nome', max_lengt=200)
     preco = models.DecimalField('Preco', max_digits=10, decimal_places=2)
+    estoque = models.IntegerField('estoque', max_digits=1000000,)
     descricao = models.TextField('Descrição', blank=True, null=True)
 
     def __str__(self):
