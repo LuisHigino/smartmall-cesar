a8615fd7f906f5b99b021ddf9185b15bd07009bc
eduardommb <lemb@cesar.school>
2026-03-28 20:11:59 -0300

    fix: remove config de url do settings

--- config/settings.py
+++ config/settings.py
@@ -102,18 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-from django.contrib import admin 
-from django.urls import path, include
-from django.conf import settings 
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
