5fd1f6181ff8890c146062fc0e0e50f1b3205332
eduardommb <lemb@cesar.school>
2026-04-27 15:05:59 -0300

    FIX: Mostra navegador durante teste

--- core/tests/conftest.py
+++ core/tests/conftest.py
@@ -20,7 +20,6 @@ def browser():
     from selenium.webdriver.chrome.options import Options
     
     options = Options()
-    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
