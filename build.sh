06f65e0d256b8af619a35129c3de20f82d41a3c9
eduardommb <lemb@cesar.school>
2026-04-30 15:30:24 -0300

    move database migrations out of build

--- build.sh
+++ build.sh
@@ -3,16 +3,3 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
-
-# Create a superuser only when credentials are explicitly configured.
-python manage.py shell -c "
-import os;
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME');
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '');
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD');
-if username and password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-"
---
+++ pre_deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -o errexit
+
+python manage.py migrate
+
+# Create a superuser only when credentials are explicitly configured.
+python manage.py shell -c "
+import os;
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME');
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '');
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD');
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+"
